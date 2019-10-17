efd5af42daf474c1de6da5f1e286e15e69fdb841
Sirik Scherer <sirikscherer@sirikmacbook.home>
2019-10-17 17:33:17 +0200

    wekende website

---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from tempfile import mkdtemp
+
+# configure application
+app = Flask(__name__)
+
+# ensure responses aren't cached
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+
+@app.route("/")
+def index():
+    """
+    Renders the login/register screen
+    """
+    return render_template("index.html")
